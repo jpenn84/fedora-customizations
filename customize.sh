4b4d3ebcdf73406cc15f9cf42c496ce4c49819f5
Joshua Penn <joshualeepenn@gmail.com>
2024-05-18 18:16:38 -0400

    Initial script commit

---
+++ customize.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# install vim
+sudo dnf -y install vim
+
+# install RPM Fusion repos
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-40.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-40.noarch.rpm
+
+# install Google Chrome and remove FireFox
+sudo dnf config-manager --set-enabled google-chrome
+sudo dnf -y install google-chrome-stable
+sudo dnf rm -y firefox
+
+# TODO: update, upgrade, & reboot. Do this manually for now.
+
+# Install fastfetch (replacement for neofetch)
+sudo dnf install -y fastfetch
+
+# Install VLC
+sudo dnf install -y vlc
+
+# Install Gimp, Inkscape, and Kalligra
+sudo dnf install -y gimp inkscape krita
+
+# install Python 3 (will likely be installed as a dependency by Gimp) and PIP
+sudo dnf install -y python3 python3-pip
+
+# TODO: numlockx doesnt work. Figure out something else
