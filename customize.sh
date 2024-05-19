49d53a42de3c5e33503fac70d90d62c29dc6aa8c
Joshua Penn <joshualeepenn@gmail.com>
2024-05-19 15:07:11 -0400

    VS code - repo & install

--- customize.sh
+++ customize.sh
@@ -11,6 +11,10 @@ git config --global core.editor "vim"
 # install RPM Fusion repos
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-40.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-40.noarch.rpm
 
+# Install VS Code repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
 # install Google Chrome and remove FireFox
 sudo dnf config-manager --set-enabled google-chrome
 sudo dnf -y install google-chrome-stable
@@ -33,6 +37,9 @@ sudo dnf install -y gimp inkscape krita
 
 # install Python 3 (will likely be installed as a dependency by Gimp) and PIP
 sudo dnf install -y python3 python3-pip
+
+# install VS Code
+sudo dnf install -y code
 
 # TODO: numlockx doesnt work. Figure out something else
 
